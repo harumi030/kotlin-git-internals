aa912951e89169e0992caa9ffc638576f9e14488
Harumi Terayama <harumi.terayama@diconium.com>
2024-12-04 13:45:04 +0100

    stage 2 completed

--- Git Internals/task/src/gitinternals/Main.kt
+++ Git Internals/task/src/gitinternals/Main.kt
@@ -1,21 +1,26 @@
 package gitinternals
-import java.io.File
 import java.io.FileInputStream;
 import java.util.zip.InflaterInputStream;
 
 fun main() {
     // write your code here
-    println("Enter git object location:")
-    val blob = readln()
-    val inputStream = FileInputStream(blob)
+    println("Enter .git directory location:")
+    val gitDirectory = readln()
+
+    println("Enter git object hash:")
+    val hash = readln()
+    val folder = hash.slice(0 until 2)
+    val rest = hash.substring(2)
+
+    val inputStream = FileInputStream("$gitDirectory/objects/$folder/$rest")
     val inflaterInputStream = InflaterInputStream(inputStream)
     val byteArray = inflaterInputStream.readAllBytes()
     val data = byteArray.toString(Charsets.UTF_8)
 
-    val parts = data.split('\u0000', limit = 2)
-    if (parts.size == 2) {
-        println(parts[0]) // Print the metadata
-        println(parts[1]) // Print the actual content
+    val header = data.split('\u0000', limit = 2)[0]
+    if (header.isNotEmpty()) {
+        val parts = header.split(" ")
+        println("type:${parts[0]} length:${parts[1]}")
     } else {
         println("Unexpected data format")
     }
